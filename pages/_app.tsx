1ff00ceeaa72e3287a3c126db5963b17c5a1cfba
ibelick <julien.thibeaut@gmail.com>
2022-09-05 22:15:34 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <title>Fflur Page | Product Designer & illustrator</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
